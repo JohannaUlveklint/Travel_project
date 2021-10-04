73bb717504c0b07c4507ab63456f397675ba39da
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-04 15:41:35 +0200

    Maked functions in main and added links in ideas.txt

---
+++ Vehicle.py
@@ -0,0 +1,16 @@
+class Vehicle:
+    @abstractmethod
+    def total_travel_time(self, distance, parking_distance):
+        pass
+
+    @abstractmethod
+    def calc_travel_time(self, distance):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
