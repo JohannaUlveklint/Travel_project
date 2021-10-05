85f456c8b079f54963e4ff1b2b833d248e41dee9
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-05 10:36:41 +0200

    Continued working with organizing into functions and classes

--- bike.py
+++ bike.py
@@ -1,13 +1,16 @@
-import Vehicle
+from vehicle import Vehicle
 
 
 class Bike(Vehicle):
-    def __init__(self, speed=0, cycling_style=None, changing_time=0):
+    def __init__(self, speed=0, user_vehicle='bike', cycling_style=None, changing_time=0, to_vehicle=0, to_end_destination=0):
         self.speed = speed
+        self.user_vehicle = user_vehicle
         self.cycling_style = cycling_style
         self.changing_time = changing_time
+        self.to_vehicle = to_vehicle
+        self.to_end_destination = to_end_destination
 
-    def time_by_bike(self, speed, distance, walking_time, cycling_style, changing_time):
+    def time_by_bike(self, distance, walking_time, cycling_style, changing_time):
         while cycling_style != 'moderate' and cycling_style != 'fast':
             cycling_style = input('Do you consider yourself a [m]oderate or [f]ast cyclist? ')
             if cycling_style == 'm':
@@ -19,7 +22,7 @@ class Bike(Vehicle):
                 speed = 20
                 changing_time = 5
                 print('The expected average speed including stopping for traffic lights will be 20 km/h.')
-                print('Five minutes for changing training clothes will be included.')
+                print('Five minutes for change from training clothes will be included.')
             else:
                 print('Please choose "m" for moderate cyclist or "f" for fast cyclist.')
         time = (distance / speed) * 60 + walking_time + changing_time
--- main.py
+++ main.py
@@ -1,44 +1,41 @@
-import Vehicle
-import Bike
-import Car
+from vehicle import Vehicle
+from bike import Bike
+from car import Car
 
-
-def travel_questions(self):
-    while user_vehicle != 'bike' and user_vehicle != 'car':
-        user_vehicle = input("Do you want to go by [b]ike or [c]ar? ")
-        if user_vehicle.lower() == 'b':
-            self.user_vehicle = 'bike'
-            print('You chose the bike.')
-        elif user_vehicle.lower() == 'c':
-            self.user_vehicle = 'car'
-            print('You chose the car.')
-        else:
-            print('Please choose "b" for bike or "c" for car.')
-    distance = int(input('How long is the distance you want to travel? Please answer in km. '))
-    to_vehicle = int(input(f'How many minutes walk is it to your {user_vehicle}? '))
-    to_end_destination = int(input('How many minutes walk is it to your end destination? '))
-    return self.user_vehicle, distance, to_vehicle, to_end_destination
-
-
-def total_time(self, user_vehicle, walking_time, distance):
-    if user_vehicle == 'bike':
-        print(f'Your total travel time is estimated to {time_by_bike(distance, walking_time)} minutes.')
-    if user_vehicle == 'car':
-        print(f'Your total travel time is estimated to {time_by_car(distance, walking_time)} minutes.')
+# def total_time(self, user_vehicle, walking_time, distance):
+#     if user_vehicle == 'bike':
+#         print(f'Your total travel time is estimated to {time_by_bike(distance, walking_time)} minutes.')
+#     if user_vehicle == 'car':
+#         print(f'Your total travel time is estimated to {time_by_car(distance, walking_time)} minutes.')
+#
 
 
 def main():
-    # Trying to get local variables inside functions with self.
-    user_vehicle = None
-    travel_questions()
+    # Get rid of all the variables below
+    user_vehicle, cycling_style = None, None
+    to_vehicle, to_end_destination, distance, walking_time, changing_time, buy_ticket_time, speed, time = \
+        0, 0, 0, 0, 0, 0, 0, 0
+    while user_vehicle != 'bike' and user_vehicle != 'car':
+        user_vehicle = input("Do you want to go by [b]ike or [c]ar? ")
+        if user_vehicle.lower() == 'b':
+            user_vehicle = 'bike'
+            vehicle = Bike()
+            print('You chose the bike.')
+        elif user_vehicle.lower() == 'c':
+            user_vehicle = 'car'
+            vehicle = Car
+            print('You chose the car.')
+        else:
+            print('Please choose "b" for bike or "c" for car.')
+
+    # How to put this as functions in classes?
+    vehicle.travel_questions(user_vehicle)
+    vehicle.to_and_from_vehicle(to_vehicle, to_end_destination)
     if user_vehicle == 'bike':
-        vehicle = Bike()
-        print(user_vehicle)
+        vehicle.time_by_bike(distance, walking_time, cycling_style, changing_time)
     elif user_vehicle == 'car':
-        vehicle = Car()
-        print(user_vehicle)
-    else:
-        print('Something got wrong')
+        vehicle.time_by_car(speed, distance, walking_time, buy_ticket_time)
+    print(f'The estimated total travel time for your journey is {time} minutes.')
 
 
 if __name__ == '__main__':
