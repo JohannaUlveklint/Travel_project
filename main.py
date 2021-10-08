77c9bedb7715c7aa4f112639bef6e5be1a4999e0
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-08 16:06:25 +0200

    Added classes in classes_and_functions.txt.

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ from travel import Travel
 def main():
     traveller1 = Travel()
     traveller1.travel_questions()
-    traveller2 = Travel()
-    traveller2.travel_questions()
     traveller1.time_by_car()  # The data is stored in the traveller obj and don´t have to be passed
     traveller1.time_by_bike()
-    traveller2.time_by_car()  # The data is stored in the traveller obj and don´t have to be passed
-    traveller2.time_by_bike()
 
 
 if __name__ == '__main__':
