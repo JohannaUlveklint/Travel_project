a9918db37b12f3e8fc68ba0068b89aca6e138ab5
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-15 16:32:57 +0200

    Updated get_lat_and_long() and started with Bing Maps Key and URL for driving route.

--- address.py
+++ address.py
@@ -1,25 +1,39 @@
 from geopy.geocoders import Nominatim
 from terminal_color import color_print
 
+"""
+https://www.bingmapsportal.com/Application
+https://docs.microsoft.com/en-us/bingmaps/rest-services/routes/calculate-a-route?redirectedfrom=MSDN
+"""
+
 
 class Address:
-    def __init__(self, latitude=None, longitude=None):
-        self.latitude = latitude
-        self.longitude = longitude
+    def __init__(self, from_latitude=None, from_longitude=None, to_latitude=None, to_longitude=None):
+        self.from_lat = from_latitude
+        self.from_long = from_longitude
+        self.to_lat = to_latitude
+        self.to_long = to_longitude
 
     def get_lat_and_long(self):
-        geolocator = Nominatim(user_agent="my_application")
-        address = input(color_print('green', 'Enter <street> <street number> <city>'))
-        location = geolocator.geocode(address)
-        self.latitude = location.latitude
-        self.longitude = location.longitude
-        return self.latitude, self.longitude  # I return two variables but it still prints 'None'
+        geolocator = Nominatim(user_agent="my_application", timeout=10)
+        from_address = input('Enter <street> <street number> <city> of your start destination: ')
+        # address = input(color_print('green', 'Enter <street> <street number> <city>'))
+        # I return two variables but it prints 'None' here, why?
+        from_location = geolocator.geocode(from_address)
+        self.from_lat = from_location.latitude
+
+        to_address = input('Enter <street> <street number> <city> of your end destination: ')
+        to_location = geolocator.geocode(to_address)
+        self.to_lat = to_location.latitude
+        self.to_long = to_location.longitude
+        return self.from_lat, self.from_long, self.to_lat, self.to_long
+
 
 def main():
     address = Address()
     address.get_lat_and_long()
-    print(address.latitude)
-    print(address.longitude)
+    bing_maps_key = 'Ao811tzo3TGlB7SiQPhGQwVg31dHk6pTH5BeSAT8swbIWbbY20aHqpkYrJf2Tsg5'
+    driving_url = "http://dev.virtualearth.net/REST/v1/Routes?wayPoint.1={wayPoint1}&viaWaypoint.2={viaWaypoint2}&waypoint.3={waypoint3}&wayPoint.n={waypointN}&heading={heading}&optimize={optimize}&avoid={avoid}&distanceBeforeFirstTurn={distanceBeforeFirstTurn}&routeAttributes={routeAttributes}&timeType={timeType}&dateTime={dateTime}&maxSolutions={maxSolutions}&tolerances={tolerances}&distanceUnit={distanceUnit}&key=" + bing_maps_key
 
 
 if __name__ == '__main__':
