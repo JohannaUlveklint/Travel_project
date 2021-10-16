f9ae56ce161353eebefeaa848e20b5b2a0e4e9bc
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-17 00:08:23 +0200

    Struggled to get info out of json but made a time converter sec_to_hh_mm_ss().

--- address.py
+++ address.py
@@ -2,6 +2,11 @@ from geopy.geocoders import Nominatim
 from terminal_color import color_print
 import urllib.request
 import json
+import openrouteservice
+from openrouteservice import convert
+import datetime
+
+
 
 """
 https://www.bingmapsportal.com/Application
@@ -31,37 +36,56 @@ class Address:
         self.to_long = str(to_location.longitude)
         return self.from_lat, self.from_long, self.to_lat, self.to_long
 
+    def seconds(self, response):
+        seconds = 0
+        for i in response:
+            if i == 'duration':
+                seconds += i
+        return seconds
+
+    def sec_to_hh_mm_ss(self, time_in_sec):
+        hh_mm_ss = (datetime.timedelta(seconds=time_in_sec))
+        return hh_mm_ss
+
 
 def main():
     address = Address()
     address.get_lat_and_long()
-    # print(address.from_lat)
-    # print(address.from_long)
-    # print(address.to_lat)
-    # print(address.to_long)
 
-    bing_maps_key = 'Ao811tzo3TGlB7SiQPhGQwVg31dHk6pTH5BeSAT8swbIWbbY20aHqpkYrJf2Tsg5'
-    car_url = "http://dev.virtualearth.net/REST/v1/Routes?wayPoint.1=" + address.from_lat + "," + address.from_long + \
-                  "&wayPoint.2=" + address.to_lat + "," + address.to_long + "&key=" + bing_maps_key
-    bike_url = "http://dev.virtualearth.net/REST/v1/Routes/Walking?wayPoint.1=" + address.from_lat + "," + \
-                  address.from_long + "&wayPoint.2=" + address.to_lat + "," + address.to_long + "&key=" \
-                  + bing_maps_key
+    #
+    # bike_request = urllib.request.Request(bike_url)
+    # bike_response = urllib.request.urlopen(bike_request)
+    # print(bike_response.read())
+    #
+    # r = car_response.read().decode(encoding="utf-8")
+    # result = json.loads(r)
+    #
+    # itinerary_items = result["resourceSets"][0]["resources"][0]["routeLegs"][0]["itineraryItems"]
+    #
+    # for item in itinerary_items:
+    #     print(item["instruction"]["text"])
+
+    api_key = "5b3ce3597851110001cf6248d62eca3e4d314dba96c2e5596a0f8074"
+    car_url = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=" + api_key + "&start=" + \
+              address.from_long + "," + address.from_lat + "&end=" + address.to_long + "," + address.to_lat + ""
+    bike_regular_url = "https://api.openrouteservice.org/v2/directions/cycling-regular?api_key=" + api_key + "&start=" +\
+                       address.from_long + "," + address.from_lat + "&end=" + address.to_long + "," + address.to_lat + ""
+    bike_electric_url = "https://api.openrouteservice.org/v2/directions/cycling-electric?api_key=" + api_key + \
+                        "&start=" + address.from_long + "," + address.from_lat + "&end=" + address.to_long + "," + address.to_lat + ""
 
     car_request = urllib.request.Request(car_url)
     car_response = urllib.request.urlopen(car_request)
-    print(car_response.read())
+    decoded = car_response.read().decode(encoding="utf-8")  # What´s the difference between decoded and result?
+    result = json.loads(decoded)
+    print(result)
+    print(decoded)
 
-    bike_request = urllib.request.Request(bike_url)
-    bike_response = urllib.request.urlopen(bike_request)
-    print(bike_response.read())
+    # ['features']['properties']['segments']['duration'] is not [1][2][0][1]: string index out of range
+    time_in_sec = decoded['features']['properties']['segments']['duration']
+    print(time_in_sec)
 
-    r = car_response.read().decode(encoding="utf-8")
-    result = json.loads(r)
 
-    itinerary_items = result["resourceSets"][0]["resources"][0]["routeLegs"][0]["itineraryItems"]
 
-    for item in itinerary_items:
-        print(item["instruction"]["text"])
 
 
 if __name__ == '__main__':
