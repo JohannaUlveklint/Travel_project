e78555e652ee5f9f16c93715384b0ab2af8c6c10
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-18 11:50:44 +0200

    Joakim fixed troubles with not picking up returned data, thank you!

--- address.py
+++ address.py
@@ -24,38 +24,43 @@ class Address:
 
     def get_lat_and_long(self):
         geolocator = Nominatim(user_agent="my_application", timeout=10)
-        from_address = input('Enter <street> <street number> <city> of your start destination: ')
+        # from_address = input('Enter <street> <street number> <city> of your start destination: ')
+        from_address = 'Övre Hallegatan 50 Göteborg'
         # address = input(color_print('green', 'Enter <street> <street number> <city>'))
         # I return two variables but it prints 'None' here, why?
         from_location = geolocator.geocode(from_address)
         self.from_lat = str(from_location.latitude)
         self.from_long = str(from_location.longitude)
 
-        to_address = input('Enter <street> <street number> <city> of your end destination: ')
+        # to_address = input('Enter <street> <street number> <city> of your end destination: ')
+        to_address = 'Hästhovsgatan 8 Göteborg'
         to_location = geolocator.geocode(to_address)
         self.to_lat = str(to_location.latitude)
         self.to_long = str(to_location.longitude)
         return self.from_lat, self.from_long, self.to_lat, self.to_long
 
-    # def seconds(self, response):
-    #     seconds = 0
-    #     for i in response:
-    #         if i == 'duration':
-    #             seconds += i
-    #     return seconds
-
-    def get_url(self, from_long=None, from_lat=None, to_long=None, to_lat=None):
+    def get_url(self):
         api_key = "5b3ce3597851110001cf6248d62eca3e4d314dba96c2e5596a0f8074"
-        search = str('api_key=' + api_key + '&start=' + from_long + ',' + from_lat + '&end=' + \
-                 to_long + ',' + to_lat)
-        car_url = requests.get(f"https://api.openrouteservice.org/v2/directions/driving-car?" + search).json()
-        reg_bike_url = requests.get(f"https://api.openrouteservice.org/v2/directions/cycling-regular?" + search).json()
-        elect_bike_url = requests.get(f"https://api.openrouteservice.org/v2/directions/cycling-electric?" + search).json()
-        return car_url, reg_bike_url, elect_bike_url
 
-    def get_distance_and_duration(self, car_url=None, reg_bike_url=None, elect_bike_url=None):
+        if not all([self.from_long, self.from_lat, self.to_long, self.to_lat]): # If any is missing values (=None)
+            print("Missing geo-coordinates")
+            return None, None, None
+        search = f'api_key={api_key}&start={self.from_long},{self.from_lat}&end={self.to_long},{self.to_lat}'
+
+        base_url = 'https://api.openrouteservice.org/v2/directions/'
+        vehicles = ['driving-car', 'cycling-regular', 'cycling-electric']
+
+        #car_url, reg_bike_url, elect_bike_url = [requests.get(f"{base_url}{v}?" + search).json() for v in vehicles]
+        # Instead of adding values to the three variables as above we can directly return the comprehension and in
+        # get_distance_and_duration() the output will be set to the variables in car_url, reg_bike_url, elect_bike_url
+        # = self.get_url()
+
+        return [requests.get(f"{base_url}{v}?" + search).json() for v in vehicles]
+
+    def get_distance_and_duration(self):
         self.get_lat_and_long()
-        self.get_url()
+        car_url, reg_bike_url, elect_bike_url = self.get_url()
+        # Make as loop?
         car_distance = car_url['features'][0]['properties']['segments'][0]['distance']
         reg_bike_distance = reg_bike_url['features'][0]['properties']['segments'][0]['distance']
         elect_bike_distance = elect_bike_url['features'][0]['properties']['segments'][0]['distance']
@@ -63,8 +68,8 @@ class Address:
         car_duration = car_url['features'][0]['properties']['segments'][0]['duration']
         reg_bike_duration = reg_bike_url['features'][0]['properties']['segments'][0]['duration']
         elect_bike_duration = elect_bike_url['features'][0]['properties']['segments'][0]['duration']
-
-        return car_distance, reg_bike_distance, elect_bike_distance, car_duration, reg_bike_duration, elect_bike_duration
+        return car_distance, reg_bike_distance, elect_bike_distance, car_duration, reg_bike_duration, \
+               elect_bike_duration
 
     @staticmethod
     def sec_to_hh_mm_ss(time_in_sec):
@@ -74,28 +79,27 @@ class Address:
 
 def main():
     address = Address()
-    address.get_distance_and_duration()
-    print(address.car_distance, address.reg_bike_distance, address.elect_bike_distance, address.car_duration,
-          address.reg_bike_duration, address.elect_bike_duration)
+    cd, rbd, ebd, cdu, rbdu, ebdu = address.get_distance_and_duration()
+    print('Car:')
+    print('====')
+    print(f'Car distance {cd}\nCar duration {cdu}')
+    print('Regular Bike:')
+    print('=============')
+    print(f'Regular bike distance {rbd}\nRegular bike duration {rbdu}')
+    print('Electirc Bike:')
+    print('==============')
+    print(f'Electric bike distance {ebd}\nElectric bike duration {ebdu}')
 
     # api_key = "5b3ce3597851110001cf6248d62eca3e4d314dba96c2e5596a0f8074"
-    # search = 'api_key=' + api_key + '&start=' + address.from_long + ',' + address.from_lat + '&end=' + \
+    # search = f'api_key=' + api_key + '&start=' + address.from_long + ',' + address.from_lat + '&end=' + \
     #          address.to_long + ',' + address.to_lat
     #
-    # url = requests.get(f"https://api.openrouteservice.org/v2/directions/" + vehicle + "?" + search).json()
+    # url = requests.get(f"https://api.openrouteservice.org/v2/directions/driving-car?" + search).json()
     # distance = url['features'][0]['properties']['segments'][0]['distance']
     # duration = url['features'][0]['properties']['segments'][0]['duration']
     # print(distance)
     # print(duration)
-    #
-    # car_url = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=" + api_key + "&start=" + \
-    #           address.from_long + "," + address.from_lat + "&end=" + address.to_long + "," + address.to_lat + ""
-    # bike_regular_url = "https://api.openrouteservice.org/v2/directions/cycling-regular?api_key=" + api_key + "&start=" +\
-    #                    address.from_long + "," + address.from_lat + "&end=" + address.to_long + "," + address.to_lat + ""
-    # bike_electric_url = "https://api.openrouteservice.org/v2/directions/cycling-electric?api_key=" + api_key + \
-    #                     "&start=" + address.from_long + "," + address.from_lat + "&end=" + address.to_long + "," + address.to_lat + ""
-    #
-    #
+
 
 if __name__ == '__main__':
     main()
