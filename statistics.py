722555d1b6de8c0b4640d0903a1ea5133a02276d
JohannaUlveklint <johanna.ulveklint@gmail.com>
2021-10-26 15:16:44 +0200

    Added method compare_weeks() to be used with matplotlib.

--- statistics.py
+++ statistics.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 from decimal import Decimal
+import matplotlib
 
 
 class Statistics:
@@ -9,6 +10,7 @@ class Statistics:
     
     def calc_distance(self):
         with open('./saved_trips/test_statistics.wifm', 'r', encoding='utf-8') as json_file:
+            # Change file later so that the user chooses which file to get data from
             data = json.load(json_file)
             meter = 0
             for line in data:
@@ -20,16 +22,41 @@ class Statistics:
         with open('./saved_trips/test_statistics.wifm', 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
             meter = 0
+            distance = 0  # Do I really need to declare distance here?
     
             for line in data:
                 if line['week'] == week:
                     meter += line['distance']
                     distance = self.m_to_km(meter)
                 # if not all line['week'] != week:  # How do I check if logs from a certain week is missing?
-                #     print('There are no logged trips from that week.')
+                #     print(f'There are no logged/saved trips from week {week}.')
                 #     break
     
             return week, distance
+    """
+    var={}
+    var[1] = 10
+    var[2] = 100
+    var[3] = 1000
+    
+    test={}
+    for i in range(1,4):
+        test[i] = var[i] +1
+    
+    print(test)
+    """
+    def compare_weeks(self):
+        num_of_weeks = int(input('How many weeks do you want to compare? '))
+        week_numbers = {}
+        for i in range(num_of_weeks):
+            week_numbers[i] = int(input(f'Type week {i + 1}: '))
+
+        week_distances = {}
+        for i in week_numbers.values():
+            week_distances[i] = self.calc_weekly_distance(i)[1]  # How do I pass the value of i to the method?
+        weeks = week_distances.keys()
+        distances = week_distances.values()
+        return weeks, distances
     
     def m_to_km(self, meter):
             km = Decimal(meter / 1000).quantize(Decimal("1.000"))
@@ -57,25 +84,23 @@ class Statistics:
 def main():
     statistics = Statistics()
     print(statistics.calc_distance())
-    week, distance = statistics.calc_weekly_distance(18)
-    print(f'Distance week {week}: {distance} km!')
-    # print(statistics.calc_weekly_distance(18)[1])
-    # print(calc_duration())
+    # week, distance = statistics.calc_weekly_distance(18)
+    # print(f'Distance week {week}: {distance} km!')
 
-    # with open('test_statistics.wifm', 'r', encoding='utf-8') as json_file:
-    #     data = json.load(json_file)
-    #     distance = 0
-    #
-    #     for line in data:
-    #         if line['week'] == '18':
-    #             distance += line['distance']
-    #             print(f'Distance week 18: {distance} km!')
-    #         elif line['week'] == '19':
-    #             distance += line['distance']
-    #             print(f'Distance week 19: {distance} km!')
-    #
+    week_distance = statistics.compare_weeks()  # Returns dict week_distance{week_number: km}
+    print(week_distance)  # {18: Decimal('39.693'), 19: Decimal('39.893')}
+    weeks = week_distance.keys()
+    distances = week_distance.values()
 
-#     data = json.load(json_file)
+    print(weeks)  # dict_keys([18, 19])
+    print(distances)  # dict_values([Decimal('39.693'), Decimal('39.893')])
+
+    for week in weeks:
+        print(week)
+
+    for distance in distances:
+        print(distance)
+
 
 
 if __name__ == '__main__':
